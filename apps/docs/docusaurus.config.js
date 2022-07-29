ede396959469cf038351c7d3452a92cabc249590
Oliver Butler <dev@oliverbutler.uk>
2022-07-29 21:07:50 +0100

    ✨ Add docusaurus deployment

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -94,10 +94,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/ts-rest/ts-rest',
               },
--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
