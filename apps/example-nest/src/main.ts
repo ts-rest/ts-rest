b7ecf992e7de56fe623300040d42c7065130b994
Oliver Butler <dev@oliverbutler.uk>
2022-08-06 22:00:18 +0100

    ✨ Start OpenAPI Gen

--- apps/example-nest/src/main.ts
+++ apps/example-nest/src/main.ts
@@ -1,10 +1,8 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { router } from '@ts-rest/example-contracts';
+import { generateOpenApi } from '@ts-rest/open-api';
 import cors = require('cors');
 import { AppModule } from './app/app.module';
 
@@ -13,6 +11,10 @@ async function bootstrap() {
   const port = process.env.PORT || 3334;
 
   app.use(cors());
+
+  const document = generateOpenApi(router);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
--- libs/ts-rest/open-api/src/lib/ts-rest-open-api.ts
+++ libs/ts-rest/open-api/src/lib/ts-rest-open-api.ts
@@ -1,3 +1,54 @@
-export function tsRestOpenApi(): string {
-  return 'ts-rest-open-api';
-}
+import { AppRouter, getAppRoutePathRoute, isAppRoute } from '@ts-rest/core';
+import { OpenAPIObject, PathsObject } from 'openapi3-ts';
+
+const getPathsFromRouter = (router: AppRouter): string[] => {
+  const paths: string[] = [];
+
+  Object.keys(router).forEach((key) => {
+    const value = router[key];
+
+    if (isAppRoute(value)) {
+      const path = getAppRoutePathRoute(value);
+
+      paths.push(path);
+    } else {
+      paths.push(...getPathsFromRouter(value));
+    }
+  });
+
+  return paths;
+};
+
+export const generateOpenApi = (router: AppRouter): OpenAPIObject => {
+  const paths = getPathsFromRouter(router);
+
+  const pathObject = paths.reduce((acc, path) => {
+    acc[path] = {
+      put: {
+        description: 'Get all',
+        responses: {
+          200: {
+            description: 'Success',
+          },
+        },
+      },
+    };
+
+    return acc;
+  }, {} as PathsObject);
+
+  console.log('done 4');
+
+  const document: OpenAPIObject = {
+    openapi: '3.0.0',
+    paths: pathObject,
+    components: { schemas: {} },
+
+    info: {
+      title: 'Example Nest',
+      version: '1.0.0',
+    },
+  };
+
+  return document;
+};
