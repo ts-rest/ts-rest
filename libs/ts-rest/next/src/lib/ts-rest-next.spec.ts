91e5eae29cf60eede50e479eb1fed35697995df2
Oliver Butler <dev@oliverbutler.uk>
2022-08-27 01:21:20 +0100

    feat: Add basic integration test

--- libs/ts-rest/next/src/lib/ts-rest-next.spec.ts
+++ libs/ts-rest/next/src/lib/ts-rest-next.spec.ts
@@ -1,7 +1,53 @@
-import { tsRestNext } from './ts-rest-next';
+import { initTsRest } from '@ts-rest/core';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createNextRoute, createNextRouter } from './ts-rest-next';
 
-describe('tsRestNext', () => {
-  it('should work', () => {
-    expect(tsRestNext()).toEqual('ts-rest-next');
+const c = initTsRest();
+
+const contract = c.router({
+  get: c.query({
+    method: 'GET',
+    path: () => '/test',
+    query: null,
+    responses: {
+      200: c.response<{ message: string }>(),
+    },
+  }),
+});
+
+const nextEndpoint = createNextRoute(contract, {
+  get: async (args) => {
+    return {
+      status: 200,
+      body: {
+        message: 'Hello World',
+      },
+    };
+  },
+});
+
+describe('createNextRouter', () => {
+  it('should send back a 200', async () => {
+    const resultingRouter = createNextRouter(contract, nextEndpoint);
+
+    const mockReq = {
+      query: {
+        ['ts-rest']: ['test'],
+      },
+      method: 'GET',
+    } as unknown as NextApiRequest;
+
+    const jsonMock = jest.fn();
+
+    const mockRes = {
+      status: jest.fn(() => ({ end: jest.fn(), json: jsonMock })),
+    } as unknown as NextApiResponse;
+
+    await resultingRouter(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(jsonMock).toHaveBeenCalledWith({
+      message: 'Hello World',
+    });
   });
 });
