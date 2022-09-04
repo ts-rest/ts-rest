526eba3516b2f83aad26450daaeac2af1316abc8
Oliver Butler <dev@oliverbutler.uk>
2022-09-05 00:57:45 +0100

    fix: rename _redirects

--- tools/scripts/docs-redirects.ts
+++ tools/scripts/docs-redirects.ts
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const redirectsFile = fs.readFileSync('apps/docs/redirects');
+const redirectsFile = fs.readFileSync('apps/docs/_redirects');
 
 // write redirects file to dist/apps/docs
-fs.writeFileSync('dist/apps/docs/redirects', redirectsFile);
+fs.writeFileSync('dist/apps/docs/_redirects', redirectsFile);
 
 console.log(
   'docs-redirects.ts > Docs redirects file created in docs dist output'
