b8709046a6dc4a107dbc4ba536389cd4c663651c
Per Hermansson <perher@netinsight.net>
2022-09-15 11:52:29 +0200

    feat: Add generateOpenApi tests

---
+++ libs/ts-rest/open-api/src/lib/ts-rest-open-api.spec.ts
@@ -0,0 +1,136 @@
+import { initContract } from '@ts-rest/core';
+import { z } from 'zod';
+import { OpenAPIObject } from 'openapi3-ts';
+import { generateOpenApi } from './ts-rest-open-api';
+
+const c = initContract();
+
+type Post = {
+  title: string;
+  published: boolean;
+};
+
+const postsRouter = c.router({
+  getPost: {
+    method: 'GET',
+    path: `/posts/:id`,
+    responses: {
+      200: c.response<Post | null>(),
+    },
+  },
+  createPost: {
+    method: 'POST',
+    path: '/posts',
+    deprecated: true,
+    responses: {
+      200: c.response<Post>(),
+    },
+    body: z.object({
+      title: z.string(),
+      published: z.boolean().optional(),
+    }),
+  },
+});
+
+const router = c.router({
+  posts: postsRouter,
+  health: {
+    method: 'GET',
+    path: '/health',
+    summary: 'Health API',
+    description: `Check the application's health status`,
+    responses: {
+      200: c.response<{ message: string }>(),
+    },
+  },
+});
+
+const expectedApiDoc = {
+  info: {
+    title: 'Blog API',
+    version: '0.1',
+  },
+  openapi: '3.0.0',
+  paths: {
+    '/health': {
+      get: {
+        deprecated: undefined,
+        description: "Check the application's health status",
+        parameters: undefined,
+        responses: {
+          '200': {
+            description: '200',
+          },
+        },
+        summary: 'Health API',
+        tags: [],
+      },
+    },
+    '/posts': {
+      post: {
+        deprecated: true,
+        description: undefined,
+        parameters: undefined,
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: {
+                additionalProperties: false,
+                properties: {
+                  published: {
+                    type: 'boolean',
+                  },
+                  title: {
+                    type: 'string',
+                  },
+                },
+                required: ['title'],
+                type: 'object',
+              },
+            },
+          },
+          description: 'Body',
+        },
+        responses: {
+          '200': {
+            description: '200',
+          },
+        },
+        summary: undefined,
+        tags: ['posts'],
+      },
+    },
+    '/posts/{id}': {
+      get: {
+        deprecated: undefined,
+        description: undefined,
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            required: true,
+          },
+        ],
+        responses: {
+          '200': {
+            description: '200',
+          },
+        },
+        summary: undefined,
+        tags: ['posts'],
+      },
+    },
+  },
+};
+
+describe('ts-rest-open-api', () => {
+  describe('generateOpenApi', () => {
+    it('w/ no parameters', async () => {
+      const apiDoc = generateOpenApi(router, {
+        info: { title: 'Blog API', version: '0.1' },
+      });
+
+      expect(apiDoc).toEqual(expectedApiDoc);
+    });
+  });
+});
