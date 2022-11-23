5b5b2e32956d461ddfca5f71417b01a8d3c7d4ff
Oliver Butler <dev@oliverbutler.uk>
2022-11-23 22:22:14 +0000

    chore: affected docs

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,6 +1,3 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
