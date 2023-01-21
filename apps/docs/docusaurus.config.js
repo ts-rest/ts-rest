9c51bf11a512bed24e5e99efa6bfc24df7cae7e2
Oliver Butler <dev@oliverbutler.uk>
2023-01-21 18:00:17 +0000

    feat: improve doc readability

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,5 +1,5 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/duotoneDark');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
