13ca71b1ec88da5f18e76e588cd2001623ac5824
Oliver Butler <dev@oliverbutler.uk>
2023-01-22 23:46:44 +0000

    feat: enable nested routers by introducing RecursivelyProcessAppRouter (#138)
    * feat: enable nested routers by introducing RecursivelyProcessAppRouter
    
    * refactor: move getValue to express lib
    
    * test: move getValue tests to express lib

--- libs/ts-rest/core/src/lib/type-utils.ts
+++ libs/ts-rest/core/src/lib/type-utils.ts
@@ -37,26 +37,6 @@ export type GetFieldType<T, P> = P extends `${infer Left}.${infer Right}`
     : undefined
   : undefined;
 
-export function getValue<
-  TData,
-  TPath extends string,
-  TDefault = GetFieldType<TData, TPath>
->(
-  data: TData,
-  path: TPath,
-  defaultValue?: TDefault
-): GetFieldType<TData, TPath> | TDefault {
-  const value = path
-    .split(/[.[\]]/)
-    .filter(Boolean)
-    .reduce<GetFieldType<TData, TPath>>(
-      (value, key) => (value as any)?.[key],
-      data as any
-    );
-
-  return value !== undefined ? value : (defaultValue as TDefault);
-}
-
 // https://stackoverflow.com/questions/63447660/typescript-remove-all-properties-with-particular-type
 // Nested solution also available ^
 type ExcludeKeysWithTypeOf<T, V> = {
@@ -73,21 +53,6 @@ export type With<T, V> = Pick<T, ExcludeKeysWithoutTypeOf<T, V>>;
 export type ZodInferOrType<T> = T extends ZodTypeAny ? z.infer<T> : T;
 
 export type Merge<T, U> = Omit<T, keyof U> & U;
-
-type Try<A, B, C> = A extends B ? A : C;
-
-type NarrowRaw<T> =
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  | (T extends Function ? T : never)
-  | (T extends string | number | bigint | boolean ? T : never)
-  | (T extends [] ? [] : never)
-  | {
-      [K in keyof T]: K extends 'description' ? T[K] : NarrowNotZod<T[K]>;
-    };
-
-type NarrowNotZod<T> = Try<T, ZodType, NarrowRaw<T>>;
-
-export type Narrow<T> = Try<T, [], NarrowNotZod<T>>;
 
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
