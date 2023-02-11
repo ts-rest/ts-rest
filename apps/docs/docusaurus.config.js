15a041a77f634b53eac9b59a642b7a48a7106ae2
Rifaldhi AW <rifaldhiaw@gmail.com>
2023-02-12 02:46:02 +0700

    docs: use icons for social media navbar (#171)

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -125,22 +125,20 @@ const config = {
           },
           {
             href: 'https://github.com/ts-rest/ts-rest',
-            label: 'GitHub',
+            className: 'header-social-link header-github-link',
+            'aria-label': 'GitHub Repository',
             position: 'right',
           },
           {
-            href: 'https://www.npmjs.com/package/@ts-rest/core',
-            label: 'NPM',
-            position: 'right',
-          },
-          {
-            href: 'https://bundlephobia.com/package/@ts-rest/core',
-            label: 'Bundlephobia',
+            href: 'https://twitter.com/ts_rest',
+            className: 'header-social-link header-twitter-link',
+            'aria-label': 'Twitter',
             position: 'right',
           },
           {
             href: 'https://discord.com/invite/2Megk85k5a',
-            label: 'Discord 🆕',
+            className: 'header-social-link header-discord-link',
+            'aria-label': 'Discord',
             position: 'right',
           },
         ],
@@ -165,12 +163,16 @@ const config = {
             title: 'Community',
             items: [
               {
+                label: 'GitHub',
+                href: 'https://github.com/ts-rest/ts-rest',
+              },
+              {
                 label: 'Twitter',
                 href: 'https://twitter.com/_oliverbutler',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/ts-rest/ts-rest',
+                label: 'Discord',
+                href: 'https://discord.com/invite/2Megk85k5a',
               },
             ],
           },
