7474a08afe84da8bb9c1550ae067805ca1efea09
Oliver Butler <dev@oliverbutler.uk>
2023-05-10 13:37:55 +0100

    chore: upgrade to nx 16

--- apps/example-next/next.config.js
+++ apps/example-next/next.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+const withNx = require('@nx/next/plugins/with-nx');
 
 /**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
