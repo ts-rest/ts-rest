14adda9bda2f68afda66469a140b4b13b3bc8d4d
Oliver Butler <dev@oliverbutler.uk>
2023-05-10 13:37:55 +0100

    chore: upgrade to nx 16

--- libs/example-microservice/util-posts-api/jest.config.ts
+++ libs/example-microservice/util-posts-api/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'example-microservice-util-posts-api',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
