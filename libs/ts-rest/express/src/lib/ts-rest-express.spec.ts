8d9dfdf6eba519c0e0e39457878a9a591f12307c
Max Brosnahan <maximilianbrosnahan@gmail.com>
2023-05-29 11:47:38 +0200

    Ensure strict mode works for express

--- libs/ts-rest/express/src/lib/ts-rest-express.spec.ts
+++ libs/ts-rest/express/src/lib/ts-rest-express.spec.ts
@@ -1,4 +1,58 @@
-import { getValue } from './ts-rest-express';
+import { initContract } from '@ts-rest/core';
+import { getValue, initServer } from './ts-rest-express';
+
+const c = initContract();
+const postsRouter = c.router({
+  getPost: {
+    method: 'GET',
+    path: `/posts/:id`,
+    responses: {
+      200: null,
+    },
+  },
+});
+
+describe('strict mode', () => {
+  it('allows unknown responses when not in strict mode', () => {
+    const cLoose = c.router({
+      posts: postsRouter,
+    });
+    const s = initServer();
+
+    s.router(cLoose, {
+      posts: {
+        getPost: async ({ params: { id } }) => {
+          return {
+            status: 201,
+            body: null,
+          };
+        },
+      },
+    });
+  });
+
+  it('does not allow unknown statuses when in strict mode', () => {
+    const cLoose = c.router(
+      {
+        posts: postsRouter,
+      },
+      { strict: true }
+    );
+    const s = initServer();
+
+    s.router(cLoose, {
+      posts: {
+        // @ts-expect-error 201 is not defined as a known response
+        getPost: async ({ params: { id } }) => {
+          return {
+            status: 201,
+            body: null,
+          };
+        },
+      },
+    });
+  });
+});
 
 describe('getValue', () => {
   it('should get one level deep', () => {
