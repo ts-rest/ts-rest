e7bcbfbf5c7b0868cf988541461cb271413cf4c9
Oliver Butler <dev@oliverbutler.uk>
2023-06-02 18:45:41 +0100

    chore: upgrade to nx 16 (#214)
    Co-authored-by: michaelangrivera <mar@michaelangrivera.com>

--- libs/ts-rest/open-api/jest.config.ts
+++ libs/ts-rest/open-api/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'ts-rest-open-api',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/ts-rest/open-api',
