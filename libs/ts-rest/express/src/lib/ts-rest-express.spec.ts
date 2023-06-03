31bc1d5d96284de42c4896f8d2d0cc6123b9a37d
Oliver Butler <dev@oliverbutler.uk>
2023-06-04 00:23:39 +0300

    express `text/html` tests

--- libs/ts-rest/express/src/lib/ts-rest-express.spec.ts
+++ libs/ts-rest/express/src/lib/ts-rest-express.spec.ts
@@ -1,5 +1,7 @@
 import { initContract } from '@ts-rest/core';
-import { initServer } from './ts-rest-express';
+import { createExpressEndpoints, initServer } from './ts-rest-express';
+import * as supertest from 'supertest';
+import * as express from 'express';
 
 const c = initContract();
 const postsRouter = c.router({
@@ -47,5 +49,83 @@ describe('strict mode', () => {
         },
       },
     });
+  });
+});
+
+describe('ts-rest-express', () => {
+  it('should allow sending non json responses', async () => {
+    const c = initContract();
+
+    const contract = c.router({
+      getIndex: {
+        method: 'GET',
+        path: `/index.html`,
+        responses: {
+          200: c.htmlResponse(),
+        },
+      },
+      getRobots: {
+        method: 'GET',
+        path: `/robots.txt`,
+        responses: {
+          200: c.textResponse(),
+        },
+      },
+      getCss: {
+        method: 'GET',
+        path: '/style.css',
+        responses: {
+          200: c.nonJsonResponse('text/css'),
+        },
+      },
+    });
+
+    const server = initServer();
+
+    const router = server.router(contract, {
+      getIndex: async () => {
+        return {
+          status: 200,
+          body: '<h1>hello world</h1>',
+        };
+      },
+      getRobots: async () => {
+        return {
+          status: 200,
+          body: 'User-agent: * Disallow: /',
+        };
+      },
+      getCss: async () => {
+        return {
+          status: 200,
+          body: 'body { color: red; }',
+        };
+      },
+    });
+
+    const app = express();
+
+    createExpressEndpoints(contract, router, app);
+
+    const responseHtml = await supertest(app).get('/index.html');
+    expect(responseHtml.status).toEqual(200);
+    expect(responseHtml.text).toEqual('<h1>hello world</h1>');
+    expect(responseHtml.header['content-type']).toEqual(
+      'text/html; charset=utf-8'
+    );
+
+    const responseTextPlain = await supertest(app).get('/robots.txt');
+    expect(responseTextPlain.status).toEqual(200);
+    expect(responseTextPlain.text).toEqual('User-agent: * Disallow: /');
+    expect(responseTextPlain.header['content-type']).toEqual(
+      'text/plain; charset=utf-8'
+    );
+
+    const responseCss = await supertest(app).get('/style.css');
+    expect(responseCss.status).toEqual(200);
+    expect(responseCss.text).toEqual('body { color: red; }');
+    expect(responseCss.header['content-type']).toEqual(
+      'text/css; charset=utf-8'
+    );
   });
 });
