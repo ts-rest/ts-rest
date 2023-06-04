e61eb0a0aa5e67f4b6c6bc845b602cea00064301
Youssef Gaber <1728215+Gabrola@users.noreply.github.com>
2023-06-04 06:15:08 +0300

    feat: add non-json responses to nest

--- libs/ts-rest/nest/src/lib/ts-rest-nest.spec.ts
+++ libs/ts-rest/nest/src/lib/ts-rest-nest.spec.ts
@@ -7,6 +7,9 @@ import {
 } from './ts-rest-nest';
 import { TsRest } from './ts-rest.decorator';
 import { TsRestRequest } from './ts-rest-request.decorator';
+import { Controller, INestApplication, Type } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as supertest from 'supertest';
 
 const c = initContract();
 const postsRouter = c.router({
@@ -83,4 +86,102 @@ it('allows responseShapes types to be used in controller logic', () => {
       return result;
     }
   }
+});
+
+describe('ts-rest-nest', () => {
+  let app: INestApplication;
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  const initializeApp = async (controller: Type) => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [controller],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+
+    return app.getHttpServer();
+  };
+
+  it('should handle non-json response types from contract', async () => {
+    const c = initContract();
+    const nonJsonContract = c.router({
+      getIndex: {
+        method: 'GET',
+        path: `/index.html`,
+        responses: {
+          200: c.htmlResponse(),
+        },
+      },
+      getRobots: {
+        method: 'GET',
+        path: `/robots.txt`,
+        responses: {
+          200: c.textResponse(),
+        },
+      },
+      getCss: {
+        method: 'GET',
+        path: '/style.css',
+        responses: {
+          200: c.nonJsonResponse('text/css'),
+        },
+      },
+    });
+
+    @Controller()
+    class NonJsonController
+      implements NestControllerInterface<typeof nonJsonContract>
+    {
+      @TsRest(nonJsonContract.getIndex)
+      async getIndex(@TsRestRequest() _: any) {
+        return {
+          status: 200,
+          body: '<h1>hello world</h1>',
+        } as const;
+      }
+
+      @TsRest(nonJsonContract.getRobots)
+      async getRobots(@TsRestRequest() _: any) {
+        return {
+          status: 200,
+          body: 'User-agent: * Disallow: /',
+        } as const;
+      }
+
+      @TsRest(nonJsonContract.getCss)
+      async getCss(@TsRestRequest() _: any) {
+        return {
+          status: 200,
+          body: 'body { color: red; }',
+        } as const;
+      }
+    }
+
+    const server = await initializeApp(NonJsonController);
+
+    const responseHtml = await supertest(server).get('/index.html');
+    expect(responseHtml.status).toEqual(200);
+    expect(responseHtml.text).toEqual('<h1>hello world</h1>');
+    expect(responseHtml.header['content-type']).toEqual(
+      'text/html; charset=utf-8'
+    );
+
+    const responseTextPlain = await supertest(server).get('/robots.txt');
+    expect(responseTextPlain.status).toEqual(200);
+    expect(responseTextPlain.text).toEqual('User-agent: * Disallow: /');
+    expect(responseTextPlain.header['content-type']).toEqual(
+      'text/plain; charset=utf-8'
+    );
+
+    const responseCss = await supertest(server).get('/style.css');
+    expect(responseCss.status).toEqual(200);
+    expect(responseCss.text).toEqual('body { color: red; }');
+    expect(responseCss.header['content-type']).toEqual(
+      'text/css; charset=utf-8'
+    );
+  });
 });
