1a0075117d428c74f2956d308379826a0fe9e3f9
Oliver Butler <dev@oliverbutler.uk>
2023-08-13 12:30:27 +0100

    fix: broken css in next-app with a parent monorepo

--- test-projects/next-app-dir/next.config.js
+++ test-projects/next-app-dir/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- test-projects/next-app-dir/tailwind.config.js
+++ test-projects/next-app-dir/tailwind.config.js
@@ -1,10 +1,8 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,5 +14,4 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
