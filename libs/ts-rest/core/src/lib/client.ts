f83b6f946473e5ae046977c93ac4165e2aa98377
Thomas F. K. Jorna <hello@tefkah.com>
2023-10-16 15:49:42 +0200

    fix: allow blob uploads to support node 18

--- libs/ts-rest/core/src/lib/client.ts
+++ libs/ts-rest/core/src/lib/client.ts
@@ -148,7 +148,7 @@ const createFormData = (body: unknown) => {
   const formData = new FormData();
 
   Object.entries(body as Record<string, unknown>).forEach(([key, value]) => {
-    if (value instanceof File) {
+    if (value instanceof Blob) {
       formData.append(key, value);
     } else {
       formData.append(key, JSON.stringify(value));
