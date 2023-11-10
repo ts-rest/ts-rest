cf0221558e41e10afba1050743cacc3cf864794f
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2023-11-10 21:30:27 +0100

    fix: routerPath does not exist anymore

--- libs/ts-rest/nest/src/lib/ts-rest-nest-handler.ts
+++ libs/ts-rest/nest/src/lib/ts-rest-nest-handler.ts
@@ -245,7 +245,7 @@ export class TsRestHandlerInterceptor implements NestInterceptor {
         return (
           doesUrlMatchContractPath(
             value.path,
-            'path' in req ? req.path : 'routeOptions' in req ? req.routeOptions.url : req.routerPath
+            'path' in req ? req.path : req.routeOptions.url
           ) && req.method === value.method
         );
       }
