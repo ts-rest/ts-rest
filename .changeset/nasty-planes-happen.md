6d7bea2e6b02f60a771d68aa813c39011891906d
Youssef Gaber <1728215+Gabrola@users.noreply.github.com>
2024-07-31 14:21:38 +0400

    chore(build): patch @nx:rollup instead of cleaning up after it builds (#656)

---
+++ .changeset/nasty-planes-happen.md
@@ -0,0 +1,5 @@
+---
+'@ts-rest/serverless': patch
+---
+
+Fix broken package exports
--- tools/scripts/rollup.config.js
+++ tools/scripts/rollup.config.js
@@ -1,8 +1,6 @@
 const fs = require('fs-extra');
-const glob = require('glob');
 const path = require('path');
 const { logger } = require('@nx/devkit');
-const { default: renameExtensions } = require('@betit/rollup-plugin-rename-extensions');
 
 let sortPackageJson;
 let builtCompleteFunction;
@@ -43,66 +41,17 @@ const rollupConfig = (config) => {
       '@ts-rest/core': peerVersion,
     };
 
-    const exportsAndTypesVersions =
-      Object.entries(packageJsonObject.exports)
-        .reduce((acc, [key, value]) => {
-          if (typeof value === 'string') {
-            acc.exports.push([key, value]);
-            return acc;
-          }
-
-          const name = value.module.replace(/\.esm\.js$/, '');
-
-          acc.exports.push([
-            key,
-            {
-              import: `${name}.esm.mjs`,
-              require: value.default,
-              types: `${name}.cjs.d.ts`
-            }
-          ]);
-
-          if (key !== '.') {
-            acc.typesVersions.push([
-              key.replace(/^\.\//, ''),
-              [`${name}.cjs.d.ts`]
-            ]);
-          }
-
-          return acc;
-        }, { exports: [], typesVersions: [] });
-
-    if (exportsAndTypesVersions.exports.length > 0) {
-      packageJsonObject.exports = Object.fromEntries(exportsAndTypesVersions.exports);
-    }
-
-    if (exportsAndTypesVersions.typesVersions.length > 0) {
-      packageJsonObject.typesVersions = { '*': Object.fromEntries(exportsAndTypesVersions.typesVersions) };
-    }
-
-    if (packageJsonObject.module) {
-      packageJsonObject.module = packageJsonObject.module.replace(/\.esm\.js$/, '.esm.mjs');
-    }
-
     fs.writeJsonSync(packageJsonPath, sortPackageJson(packageJsonObject), { spaces: 2 });
 
     logger.info(
       '\nAdded @ts-rest/core to peer dependencies',
     );
-
-    glob
-      .sync([
-        path.join(outputDir, '**/*.cjs.default.js'),
-        path.join(outputDir, '**/*.cjs.mjs')
-      ])
-      .forEach((file) => {
-        fs.rmSync(file);
-      });
   };
 
   return {
     ...config,
     external: (source, importer, isResolved) => {
+      // stop rollup from looking for @ts-rest/* because it thinks they're not external since they are not in package.json
       if (source.startsWith('@ts-rest/')) {
         return true;
       }
@@ -110,12 +59,6 @@ const rollupConfig = (config) => {
     },
     plugins: [
       ...config.plugins,
-      renameExtensions({
-        include: [`**/*.ts`, `**/*.tsx`],
-        mappings: {
-          '.esm.js': '.esm.mjs'
-        }
-      }),
     ]
   }
 }
