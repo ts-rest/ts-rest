991b8d8f66070e79903dce02fef92d1160fa9d91
Shawn Mclean <shawnrmclean@gmail.com>
2024-08-09 11:30:48 -0500

    update next example to use getMetadata

--- apps/example-next/app/react-query-utils/tsr.ts
+++ apps/example-next/app/react-query-utils/tsr.ts
@@ -2,6 +2,6 @@ import { initTsrReactQuery } from '@ts-rest/react-query/v5';
 import { testContract } from '../../contracts/test-contract';
 
 export const tsr = initTsrReactQuery(testContract, {
-  baseUrl: 'http://localhost:4200/api/edge',
+  baseUrl: 'http://localhost:4200/api/app-router',
   baseHeaders: {},
 });
