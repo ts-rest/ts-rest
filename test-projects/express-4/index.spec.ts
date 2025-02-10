a3b16e21baa378ba89815b3e35c1833271a039ea
Oliver Butler <dev@oliverbutler.uk>
2025-02-10 09:56:39 +0000

    test: add express 4 e2e

---
+++ test-projects/express-4/index.spec.ts
@@ -0,0 +1,63 @@
+import { describe, it, expect } from 'bun:test';
+import { app } from './index';
+import request from 'supertest';
+
+const expectRes = (res: any) => {
+  return expect({ body: res.body, status: res.status });
+};
+
+describe('express 4', () => {
+  it('should be able to get', async () => {
+    const res = await request(app).get('/pokemon/1');
+
+    expectRes(res).toStrictEqual({
+      status: 200,
+      body: {
+        id: 1,
+        name: 'Charizard',
+      },
+    });
+  });
+
+  it('should do param validation', async () => {
+    const res = await request(app).get('/pokemon/foo');
+
+    expectRes(res).toStrictEqual({
+      status: 400,
+      body: {
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'number',
+            message: 'Expected number, received nan',
+            path: ['id'],
+            received: 'nan',
+          },
+        ],
+        name: 'ZodError',
+      },
+    });
+  });
+
+  it('delete with no body and no return body', async () => {
+    const res = await request(app).delete('/pokemon/1');
+
+    expectRes(res).toStrictEqual({
+      status: 200,
+      body: {},
+    });
+  });
+
+  it('update', async () => {
+    const res = await request(app)
+      .patch('/pokemon/1')
+      .send({ name: 'pikachu' });
+
+    expectRes(res).toStrictEqual({
+      status: 200,
+      body: {
+        message: 'updated 1 to pikachu',
+      },
+    });
+  });
+});
