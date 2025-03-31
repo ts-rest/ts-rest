adcc3560d97352ad31c761199559f34e5bfd63bb
Piotr Zurek <pzurek@hey.com>
2025-03-31 21:53:06 +1300

    Add support for handling ALBEvents in the ts-rest-lambda handler

--- libs/ts-rest/serverless/src/lib/handlers/ts-rest-lambda.ts
+++ libs/ts-rest/serverless/src/lib/handlers/ts-rest-lambda.ts
@@ -1,26 +1,41 @@
-import type { Context } from 'aws-lambda';
-import { AppRouter } from '@ts-rest/core';
-import {
+import type { ALBEvent, Context } from 'aws-lambda';
+import type { AppRouter } from '@ts-rest/core';
+import type {
   ApiGatewayEvent,
   ApiGatewayResponse,
-  requestFromEvent,
-  responseToResult,
 } from '../mappers/aws/api-gateway';
-import { createServerlessRouter } from '../router';
 import {
-  createTsr,
+  isV2,
+  requestFromEvent as apiGatewayRequestFromEvent,
+  responseToResult as apiGatewayResponseToResult,
+} from '../mappers/aws/api-gateway';
+import type {
+  AlbEvent,
+  AlbResponse,
+} from '../mappers/aws/alb';
+import {
+  isAlbEvent,
+  requestFromEvent as albRequestFromEvent,
+  responseToResult as albResponseToResult,
+} from '../mappers/aws/alb';
+import { createServerlessRouter } from '../router';
+import type {
   RouterImplementationOrFluentRouter,
   ServerlessHandlerOptions,
 } from '../types';
+import { createTsr } from '../types';
+
+type EventType = ApiGatewayEvent | AlbEvent;
+type ResponseType = ApiGatewayResponse | AlbResponse;
 
 type LambdaPlatformArgs = {
-  rawEvent: ApiGatewayEvent;
+  rawEvent: EventType;
   lambdaContext: Context;
 };
 
 export const tsr = createTsr<LambdaPlatformArgs>();
 
-export type LambdaHandlerOptions<TRequestExtension = {}> =
+export type LambdaHandlerOptions<TRequestExtension = Record<string, unknown>> =
   ServerlessHandlerOptions<LambdaPlatformArgs, TRequestExtension>;
 
 export const createLambdaHandler = <T extends AppRouter, TRequestExtension>(
@@ -39,18 +54,22 @@ export const createLambdaHandler = <T extends AppRouter, TRequestExtension>(
   >(contract, router, options);
 
   return async (
-    event: ApiGatewayEvent,
+    event: EventType,
     context: Context,
-  ): Promise<ApiGatewayResponse> => {
-    const request = requestFromEvent(event);
+  ): Promise<ResponseType> => {
+    const request = isAlbEvent(event)
+      ? albRequestFromEvent(event as AlbEvent)
+      : apiGatewayRequestFromEvent(event as ApiGatewayEvent);
 
-    return serverlessRouter
-      .fetch(request, {
-        rawEvent: event,
-        lambdaContext: context,
-      })
-      .then(async (response) => {
-        return responseToResult(event, response);
-      });
+    const response = await serverlessRouter.fetch(request, {
+      rawEvent: event,
+      lambdaContext: context,
+    });
+
+    if (isAlbEvent(event)) {
+      return albResponseToResult(response);
+    }
+
+    return apiGatewayResponseToResult(event as ApiGatewayEvent, response);
   };
 };
