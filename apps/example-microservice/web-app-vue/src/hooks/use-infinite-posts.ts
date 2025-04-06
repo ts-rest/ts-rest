8ba1a0493aefaceb9e2a0c6caa6c14a66af947d2
Benjamin Montazer <benjamin@montazer.dev>
2025-04-06 19:57:52 +0200

    feat(vue-query): add more usecases to vue example app

--- apps/example-microservice/web-app-vue/src/hooks/use-infinite-posts.ts
+++ apps/example-microservice/web-app-vue/src/hooks/use-infinite-posts.ts
@@ -2,10 +2,10 @@ import { computed } from 'vue';
 import { useClient } from '../api/client';
 
 export const useInfinitePosts = () => {
-  const PAGE_SIZE = 5;
+  const PAGE_SIZE = 3;
 
   const { data, ...rest } = useClient().getPosts.useInfiniteQuery({
-    queryKey: ['infinite-posts'],
+    queryKey: ['infinite-posts'] as const,
     queryData: ({ pageParam }) => ({
       query: {
         skip: pageParam.skip.toString(),
--- apps/example-microservice/web-app-vue/src/index.ts
+++ apps/example-microservice/web-app-vue/src/index.ts
@@ -1,5 +1,48 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { TsRestPlugin } from './api/client';
+import { TsRestPlugin, useClient } from './api/client';
+import { createRouter, createWebHistory } from 'vue-router';
+import { apiBlog } from '@ts-rest/example-contracts';
+import { z } from 'zod';
 
-createApp(App).use(TsRestPlugin).mount('#app');
+const PostSchema = apiBlog.getPost.responses[200]
+  .omit({ id: true })
+  .merge(
+    z.object({ published: z.string().or(z.boolean()).transform(Boolean) }),
+  );
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: () => import('./components/Posts.vue') },
+    { path: '/infinite', component: () => import('./components/Infinite.vue') },
+    {
+      path: '/posts/:id',
+      name: 'single-post',
+      component: () => import('./components/Post.vue'),
+      // pass query params as props to page-component
+      props: (route) => ({
+        id: route.params.id,
+        ...route.query,
+      }),
+      beforeEnter: async (to, _from, next) => {
+        if (PostSchema.safeParse(to.query).success) return next(true); // prevent infinite-loop
+
+        const post = await useClient().getPost.query({
+          params: { id: to.params.id as string },
+        });
+
+        if (post.status !== 200) return next(false); // redirect to error page
+
+        const { id, ...body } = post.body;
+
+        return next({
+          ...to,
+          query: { ...to.query, ...body },
+        });
+      },
+    },
+  ],
+});
+
+createApp(App).use(TsRestPlugin).use(router).mount('#app');
