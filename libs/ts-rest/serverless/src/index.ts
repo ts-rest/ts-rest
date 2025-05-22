3313b33f006fe94598681a907dcb9d1f0aa2b61a
Aidan Temple <15520814+aidant@users.noreply.github.com>
2025-05-23 04:14:18 +1000

    fix(serverless): inferred types not portable (#795)
    Co-authored-by: Oliver Butler <dev@oliverbutler.uk>

--- libs/ts-rest/serverless/src/index.ts
+++ libs/ts-rest/serverless/src/index.ts
@@ -1,5 +1,14 @@
 export * from './lib/http-error';
 export * from './lib/request';
 export * from './lib/response';
-export { RequestValidationError, ResponseValidationError } from './lib/types';
+export {
+  RequestValidationError,
+  ResponseValidationError,
+  type AppRouteImplementation,
+  type AppRouteImplementationOrOptions,
+  type AppRouteOptions,
+  type RouterImplementation,
+  type RouterImplementationOrFluentRouter,
+  type ServerlessHandlerOptions,
+} from './lib/types';
 export { RouterBuilder, CompleteRouter } from './lib/router-builder';
