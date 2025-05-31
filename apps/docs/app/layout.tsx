0f61acad30c869d3239387f909983b079993628c
Oliver Butler <dev@oliverbutler.uk>
2025-05-31 21:41:45 +0100

    docs: favicon meta and icon on homepage

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,10 +1,16 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: 'ts-rest',
+  description: 'ts-rest is a library for building REST APIs with TypeScript.',
+};
 
 export default function Layout({ children }: { children: any }) {
   return (
