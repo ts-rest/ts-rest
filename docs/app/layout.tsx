9f2c875578b05c713e33f460f01187e005e5e490
Oliver Butler <dev@oliverbutler.uk>
2025-06-01 15:35:41 +0100

    docs: RC title

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,6 +2,7 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Banner } from 'fumadocs-ui/components/banner';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -16,6 +17,10 @@ export default function Layout({ children }: { children: any }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner variant="rainbow" id="3-53-0-release">
+          Zod 4 (and all other Standard Schema support) is available as a
+          release candidate `3.53.0-rc.0`
+        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
