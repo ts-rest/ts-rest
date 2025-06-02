01ac61206703dade7b104547692fc988313306ac
Oliver Butler <dev@oliverbutler.uk>
2025-06-02 08:37:42 +0100

    docs: add posthog for basic web analytics (#808)

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,6 +3,8 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import { Banner } from 'fumadocs-ui/components/banner';
+import { PostHogProvider } from '../components/PostHogProvider';
+import { CookieBanner } from '../components/CookieBanner';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -17,11 +19,14 @@ export default function Layout({ children }: { children: any }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Banner variant="rainbow" id="3-53-0-release">
-          Zod 4 (and all other Standard Schema support) is available as a
-          release candidate `3.53.0-rc.0`
-        </Banner>
-        <RootProvider>{children}</RootProvider>
+        <PostHogProvider>
+          <Banner variant="rainbow" id="3-53-0-release">
+            Zod 4 (and all other Standard Schema support) is available as a
+            release candidate `3.53.0-rc.0`
+          </Banner>
+          <RootProvider>{children}</RootProvider>
+          <CookieBanner />
+        </PostHogProvider>
       </body>
     </html>
   );
