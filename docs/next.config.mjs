334cf80e0008697176e2a3d460d09e0ff68d01e0
Oliver Butler <dev@oliverbutler.uk>
2025-06-02 08:10:49 +0100

    feat: add posthog

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -6,6 +6,24 @@ const withMDX = createMDX();
 const config = {
   serverExternalPackages: ['typescript', 'twoslash'],
   reactStrictMode: true,
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://eu.i.posthog.com/decide',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
